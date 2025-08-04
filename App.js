062df441d72e252f082b2e89803a524fcc590fe1
Ashis28 <dashashiskumar28@example.com>
2025-08-04 21:37:25 +0530

    Flast List and its components

--- App.js
+++ App.js
@@ -1,27 +1,52 @@
-//we learn how to render  a list of items in rn using map function and scrollView
-
-import { StyleSheet, Text, View, ScrollView , StatusBar } from 'react-native';
+//rendering the items using list is not a good approach as it print all the id , so will be a big problem for largetDataset
+//FlatList (it render the items currently in the view)
+//also it generates few advance components as ensure smooth operation
+import { StyleSheet, Text, View, ScrollView , StatusBar, SafeAreaView ,FlatList} from 'react-native';
 import pokemonList from './data.json'
 
 export default function App() {
   return (
-    <ScrollView style={styles.scrollView}>
-      <View style={styles.container}>
-      {
-        pokemonList.map((pokemon)=>{
-          return(
+    // <ScrollView style={styles.scrollView}>
+    //   <View style={styles.container}>
+    //   {
+    //     pokemonList.map((pokemon)=>{
+    //       return(
             
-            <View id='{pokemon.id}' style = {styles.card}>
-              <Text>{pokemon.type}</Text>
-              <Text>{pokemon.name}</Text>
-            </View>
+    //         <View id='{pokemon.id}' style = {styles.card}>
+    //           <Text>{pokemon.type}</Text>
+    //           <Text>{pokemon.name}</Text>
+    //         </View>
+    //       )
+    //     })
+    //   }
+    //   <StatusBar style="auto" />
+    // </View>
+    // </ScrollView>
+    <SafeAreaView>
+      <FlatList 
+      data = {pokemonList}
+      renderItem = {({item})=>{
+        console.log(item.id);
+        return(
+          <View id='{item.id}' style = {styles.card}>
+              
+               <Text>{item.type}</Text>
+               <Text>{item.name}</Text>
+          </View>
+        )
+      }}
+      ItemSeparatorComponent = {
+        () => {
+          return(
+            <View style = {styles.separator}></View>
           )
-        })
-      }
-      <StatusBar style="auto" />
-    </View>
-    </ScrollView>
-    
+        }
+      } 
+      ListEmptyComponent={<Text>No data found</Text>}
+      ListHeaderComponent={<Text style = {styles.listHeader}>List of Pokemon</Text>}
+      ListFooterComponent={<Text>PokemonList</Text>}
+    />
+    </SafeAreaView>
   );
 }
 
@@ -36,9 +61,18 @@ const styles = StyleSheet.create({
   },
   card : {
     borderRadius:8,
-    marginBottom:16,
+    //marginBottom:16,
     borderWidth:1,
     padding:16,
+  },
+  separator : {
+    height:16,
+    backgroundColor:'#e0e0e0',
 
-  }
+  },
+  listHeader : {
+    margin:10,
+    textAlign:'center',
+    fontSize:24,
+  },
 });
