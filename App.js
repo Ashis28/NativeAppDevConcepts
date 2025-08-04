f3c04af846844f3be7890e64ba892ff7e670d79c
Ashis28 <dashashiskumar28@example.com>
2025-08-04 23:08:32 +0530

    TextInput Successifully Done

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View , SafeAreaView , TextInput } from 'react-native';
+import { useState } from 'react';
 export default function App() {
+
+  let [name,setName] = useState("");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput style = {styles.input} value={name} onChangeText={setName} placeholder='email@gama.com' ></TextInput>
+      <Text>Hello mr : {name} </Text>
+      
+      <TextInput style = {[styles.input,styles.multiline]} placeholder='write a message' multiline></TextInput>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    paddingTop: StatusBar.currentHeight,
   },
+  input : {
+    height: 40,
+    margin: 12,
+    padding: 10,
+    borderWidth: 1, 
+  },
+
+  multiline: {
+    minHeight: 100,
+    textAlignVertical:"top", 
+  }
 });
