efed646df035e8667c6370394ab1d0d018576baa
Ashis28 <dashashiskumar28@example.com>
2025-08-04 20:10:20 +0530

    Rendering using map added

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//we learn how to render  a list of items in rn using map function and scrollView
+
+import { StyleSheet, Text, View, ScrollView , StatusBar } from 'react-native';
+import pokemonList from './data.json'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ScrollView style={styles.scrollView}>
+      <View style={styles.container}>
+      {
+        pokemonList.map((pokemon)=>{
+          return(
+            
+            <View id='{pokemon.id}' style = {styles.card}>
+              <Text>{pokemon.type}</Text>
+              <Text>{pokemon.name}</Text>
+            </View>
+          )
+        })
+      }
       <StatusBar style="auto" />
     </View>
+    </ScrollView>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    paddingTop: StatusBar.currentHeight,
   },
+  scrollView: {
+    paddingHorizontal : "16",
+  },
+  card : {
+    borderRadius:8,
+    marginBottom:16,
+    borderWidth:1,
+    padding:16,
+
+  }
 });
