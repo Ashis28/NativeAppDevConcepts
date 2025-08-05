50ef553ddafc9f885f550da9d75b7935b7ba0f1e
Ashis28 <dashashiskumar28@example.com>
2025-08-05 12:31:47 +0530

    Good project button onchange useState update and many more

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, Button, TextInput, TextInputComponent } from 'react-native';
 import { SafeAreaView, StyleSheet, Text, View, FlatList } from 'react-native';
 
 // Error 1: FlatList should be imported from 'react-native', not 'react-native-web'.
@@ -14,6 +14,10 @@ export default function App() {
   const [postList, setPostList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [refreshing,setRefresh] = useState(false);
+  const [postTitle,setPostTitle] = useState("");
+  const [postBody,setPostBody] = useState("");
+  const [isPosting,setIsPosting] = useState(false);
+  const [error, setError] = useState("");
 
   const fetchData = async (limit = 10) => {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
@@ -37,8 +41,55 @@ export default function App() {
       </SafeAreaView>
     )
   }
+
+  const addPost = async() => {
+    setIsPosting(true);
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: postTitle,
+            body: postBody,
+          }),
+        }
+      );
+      const newPost = await response.json();
+      setPostList([newPost, ...postList]);
+      setPostTitle("");
+      setPostBody("");
+      setError("");
+    }
+    catch(error){
+      console.error("Error fetching data:", error);
+      setIsLoading(false);
+      setError("Failed to fetch post list.");
+    }
+    setIsPosting(false);
+  }
   return (
     <SafeAreaView style={styles.container}>
+
+      <View>
+        <TextInput style={styles.input}
+        placeholder='post heading'
+        value={postTitle} 
+        onChangeText={setPostTitle}/>
+
+        <TextInput style={styles.input}
+        placeholder='post content'
+        value={postBody} 
+        onChangeText={setPostBody}/>
+
+        <Button title={isPosting?"..adding":"Add Post"}
+        onPress={addPost}
+        disabled={isPosting}/>
+         
+      </View>
       <View style={styles.listContainer}>
         <FlatList
           data={postList}
@@ -113,4 +164,12 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginTop: 12,
   },
+
+  input: {
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 6,
+    borderWidth: 2
+  }
 });
